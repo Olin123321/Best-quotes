32cfecb47ba79fc5fe2ceed7047900e5c9911119
Olin <61127141+Olin123321@users.noreply.github.com>
2022-04-17 16:28:44 +0200

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QUOTES } from './models/database';
+import { Quotation } from './models/quotation';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'best-quotes';
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {
+    author: '',
+    sentence: '',
+    votes: 0
+  };
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuote() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {
+      author: '',
+      sentence: '',
+      votes: 0
+    };
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
 }
